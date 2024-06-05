e1df040fb4f94e0a5cf7c052a2e0d54311fa1b33
フランカツ!! <105261832+FlaanKatsu@users.noreply.github.com>
2024-06-05 12:30:22 -0700

    done
    done

--- assignment.py
+++ assignment.py
@@ -1,40 +1,50 @@
 import tkinter as wm
 import playsound as au
 import os
+from tkinter import *
 
 #create sounds 
 def badapple(event):
-    au.playsound("au/badapple.mp3")
+    au.playsound("au/badapple.mp3",block=False)
 def superidol(event):
-    au.playsound("au/superidol.mp3")
+    au.playsound("au/superidol.mp3",block=False)
 def coffindance(event):
-    au.playsound("au/coffindance.mp3")
+    au.playsound("au/coffindance.mp3",block=False)
 def dokidoki(event):
-    au.playsound("au/necromantic.mp3")
+    au.playsound("au/necromantic.mp3",block=False)
 def windowsdie(event):
     au.playsound("au/death.mp3")
     os.system("shutdown /s")
 def emotional(event):
-    au.playsound("au/damage.mp3")
+    au.playsound("au/damage.mp3",block=False)
 def meow(event):
-    au.playsound("au/meow.mp3")
+    au.playsound("au/meow.mp3",block=False)
 def samnt(event):
-    au.playsound("au/samsung_notif.mp3")
+    au.playsound("au/samsung_notif.mp3",block=False)
 
 root = wm.Tk()
 root.attributes()
 root.geometry()
 root.title("function board")
+root.iconphoto(False,PhotoImage(file="img/icon.png"))
 
-bba = wm.Button(root,text="Bad Apple!!",width=15)
-bsi = wm.Button(root,text="热爱105°C的你",width=15)
-bcd = wm.Button(root,text="Coffin Dance",width=15)
-ddww = wm.Button(root,text="ドキドキワクワク",width=15)
+superidolpng = PhotoImage(file="img/superidol.png")
+dokiwakupng = PhotoImage(file="img/dokidokiwakuwaku.png")
+coffinpng = PhotoImage(file="img/coffin.png")
+badapplepng = PhotoImage(file="img/badapple.png")
+scratchpng = PhotoImage(file="img/scratch.png")
+damagepng = PhotoImage(file="img/damage.png")
+smagsmugpng = PhotoImage(file="img/smagsmug.png")
+
+bba = wm.Button(root,image=badapplepng,)
+bsi = wm.Button(root,image=superidolpng)
+bcd = wm.Button(root,image=coffinpng)
+ddww = wm.Button(root,image=dokiwakupng)
 kill = wm.Button(root,text="shutdown windows",width=15)
-ed = wm.Button(root,text="Emotional Damage",width=15)
-mw = wm.Button(root,text="Meow",width=15)
-smnt = wm.Button(root,text="Samsung Notification",width=15)
-
+ed = wm.Button(root,image=damagepng)
+mw = wm.Button(root,image=scratchpng)
+smnt = wm.Button(root,image=smagsmugpng)
+                 
 txt曲 = wm.Label(root,text="Songs")
 txtsfx = wm.Label(root,text="SFX")
 txtfn = wm.Label(root,text="System Functions")
